b4e9d6977b18d9c7ef31f2ed80c3b2279f1fcbb4
muhammed burak şentürk <meftunca@muhammed-MacBook-Pro.local>
2019-01-05 04:09:23 +0300

    message

--- server.js
+++ server.js
@@ -4,3 +4,6 @@ const express = require("express");
 var app = expresss();
 var path = require("path");
 app.use(express.static(path.join(__dirname, "/public")));
+app.get("/", function(req, res) {
+  res.sendFile("/index.html"); //I've tried playing around with the path multiple ways and still get errors
+});
