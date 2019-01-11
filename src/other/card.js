ba9fbb62afaeb22f83950d9b49cceff79746140b
muhammed burak şentürk <meftunca@muhammed-MacBook-Pro.local>
2019-01-11 15:50:03 +0300

    x

--- src/other/card.js
+++ src/other/card.js
@@ -1,14 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
-import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
-import PlayArrowIcon from "@material-ui/icons/PlayArrow";
-import SkipNextIcon from "@material-ui/icons/SkipNext";
+
 import { Editor } from "react-draft-wysiwyg";
 const styles = theme => ({
   card: {
