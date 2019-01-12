8eb91cb5f26d67099807696a187604314d5b5cfb
muhammed burak şentürk <meftunca@muhammed-MacBook-Pro.local>
2019-01-12 03:25:44 +0300

    x

--- server/mongo/index.js
+++ server/mongo/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose"),
   schemaCreator = require("./mongoSchemaCreator");
 console.log(process.env.MONGODB_URI);
 mongoose.connect(
-  process.env.MONGOLAB_URI || "mongodb://meftunca:meftunca12@ds153974.mlab.com:53974/heroku_zbz4gnp0",
+  process.env.MONGODB_URI || "mongodb://meftunca:meftunca12@ds153974.mlab.com:53974/heroku_zbz4gnp0",
   { useNewUrlParser: true }
 );
 mongoose.connection.on("error", function(err) {
